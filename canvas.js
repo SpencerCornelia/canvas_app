40279e65e1f70921976481d88e61fd2aaed0fabf
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-02-07 20:26:40 -0800

    added form on index.html

--- canvas.js
+++ canvas.js
@@ -1,9 +1,4 @@
 window.onload = function() {
 	var canvas = document.getElementById("tshirtCanvas");
-	if (canvas.getContext) { 
-		var context = canvas.getContext("2d");
-	} else {
-		//no browser API support
-	}	
-	context.fillRect(10, 10, 100, 100);
+	
 }
